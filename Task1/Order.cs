af6bddc986fff239fd8ca305c28fb7a2960c9925
angelina-moshkina <angelina.silich1996@gmail.com>
2023-11-26 12:33:04 +0100

    first task

---
+++ Task1/Order.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Task1
+{
+    internal class Order
+    {
+        private string _productName;
+        public string ProductName 
+        { 
+            get
+            {
+                return _productName;
+            }
+            set 
+            {
+                _productName = value;
+            }
+        }
+
+        private float _productPrice;
+        public float ProductPrice 
+        { 
+            get
+            {
+                return _productPrice;
+            }
+            set
+            {
+                if (value > 0 && value < 1000) _productPrice = value;
+            }
+        }
+        private long _buyerPhoneNumber;
+        public long BuyerPhoneNumber 
+        {
+            get 
+            {
+                return _buyerPhoneNumber;
+            }
+            set 
+            {
+                if(value.ToString().Length == 13) _buyerPhoneNumber = value;
+            }
+        }
+        
+        private string _buyerAddress;
+        public string BuyerAddress
+        {
+            get
+            {
+                return _productName;
+            }
+            set
+            {
+                _productName = value;
+            }
+        }
+
+        public Order(string productName, float productPrice, long buyerPhoneNumber, string buyerAddress)
+        {
+            _productName = productName;
+            _productPrice = productPrice;
+            _buyerPhoneNumber = buyerPhoneNumber;
+            _buyerAddress = buyerAddress;
+        }
+
+        public string GetFullOrderInfo()
+        {
+            return String.Format("Product Name-{0}, Product Price-{1}, Buyer Phone Number-{2}, buyer Address-{3}", _productName, ProductPrice,
+                                                                                                                   _buyerPhoneNumber, _buyerAddress);
+        }
+
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static bool CheckIfStringContainsText(string inputValue, string compareValue)
+        {
+            bool result = inputValue.StartsWith(compareValue);
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            Order order1 = new Order ("Whyskas", 13.50f, 3753361728362, "Gagarina 150 / 250");
+            Order order2 = new Order("Whiskas", 15.70f, 3752961728361, "Voronicha 26/140");
+            Order order3 = new Order("Kiteket", 99.90f, 5441234599, "Mira 10/132");
+            Order order4 = new Order("Purina", 20.90f, 5441234599, "Mira 10/132");
+
+            Order[] orders = { order1, order2, order3, order4 };
+
+            order1.ProductPrice = 10001f;
+            order1.BuyerPhoneNumber = 012345678901234;
+            order3.BuyerPhoneNumber = 3753361728361;
+
+            //TASK_1.2 вывести в цикле на экран полную информацию о всех заказах в массиве
+            foreach (Order item in orders)
+            { 
+                    Console.WriteLine("TASK_1.2 " + item.GetFullOrderInfo());
+            }
+
+            //TASK_2.4 Вывести на экран полную информацию о заказах, телефонный номер заказчика которых начинается на 375.
+            foreach (Order item in orders)
+            {
+                if (CheckIfStringContainsText(item.BuyerPhoneNumber.ToString(), "375") == true)
+                {
+                    Console.WriteLine("TASK_2.4 " + item.GetFullOrderInfo());
+                }
+            }
+
+            //TASK_2.5 Вывести на экран полную информацию о заказах, стоимость которых не превышает 20 и имя товара начинается на "Whys".
+            foreach (Order item in orders)
+            {
+                if (item.ProductPrice < 20 && CheckIfStringContainsText(item.ProductName, "Whys") == true)
+                {
+                    Console.WriteLine("TASK_2.5 " + item.GetFullOrderInfo());
+                }
+            }
+            
+        }
+    }
+}
